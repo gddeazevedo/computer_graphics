8db0ca58bb16bf5334149e658c2e68f38f5e7e46
gddeazevedo <gabrielcapuerj@gmail.com>
2026-03-19 22:07:33 -0300

    refactor key handling and improve cube rendering colors in 3D house model

--- aulas/house_3d_trab.cpp
+++ aulas/house_3d_trab.cpp
@@ -5,9 +5,13 @@
 #define X_KEY 'x'
 #define Y_KEY 'y'
 #define Z_KEY 'z'
+#define Q_KEY 'q'
+#define R_KEY 'r'
 #define X_KEY_UPPER 'X'
 #define Y_KEY_UPPER 'Y'
 #define Z_KEY_UPPER 'Z'
+#define Q_KEY_UPPER 'Q'
+#define R_KEY_UPPER 'R'
 #define rotate_x_axis(angle) glRotatef(angle, 1.0f, 0.0f, 0.0f) // rotaciona em torno do eixo (1, 0, 0) (eixo x) por um angulo de angle graus
 #define rotate_y_axis(angle) glRotatef(angle, 0.0f, 1.0f, 0.0f) // rotaciona em torno do eixo (0, 1, 0) (eixo y) por um angulo de angle graus
 #define rotate_z_axis(angle) glRotatef(angle, 0.0f, 0.0f, 1.0f) // rotaciona em torno do eixo (0, 0, 1) (eixo z) por um angulo de angle graus
@@ -35,7 +39,7 @@ int main(int argc, char **argv) {
     glutKeyboardFunc(handle_key_pressed);
     initGL();
     glutMainLoop();
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 void initGL() {
@@ -48,60 +52,68 @@ void initGL() {
 }
 
 void draw_cube() {
+    const float red   = 0.5f;
+    const float green = 0.5f;
+    const float blue  = 0.5f;
+
     // (x, y, z) do cubo
     glTranslatef(0.0, -1.0, 0.0);
 
     glBegin(GL_QUADS);
         // Top face (y = 1.0f)
-        glColor3f(0.0f, 0.0f, 0.0f);
+        glColor3f(red, green, blue);
 
-        glVertex3f( 1.0f, 1.0f,  1.0f);
-        glVertex3f(-1.0f, 1.0f,  1.0f);
-        glVertex3f(-1.0f, 1.0f, -1.0f);
-        glVertex3f( 1.0f, 1.0f, -1.0f);
+        glVertex3f( 1.0f, 1.0f,  2.0f);
+        glVertex3f(-1.0f, 1.0f,  2.0f);
+        glVertex3f(-1.0f, 1.0f, -2.0f);
+        glVertex3f( 1.0f, 1.0f, -2.0f);
 
         // Bottom face (y = -1.0f)
-        glColor3f( 1.0f, 1.0f, 1.0f);
-        glVertex3f( 1.0f, -1.0f,  1.0f);
-        glVertex3f(-1.0f, -1.0f,  1.0f);
-        glVertex3f(-1.0f, -1.0f, -1.0f);
-        glVertex3f( 1.0f, -1.0f, -1.0f);
+        glColor3f(red, green, blue);
+
+        glVertex3f( 1.0f, -1.0f,  2.0f);
+        glVertex3f(-1.0f, -1.0f,  2.0f);
+        glVertex3f(-1.0f, -1.0f, -2.0f);
+        glVertex3f( 1.0f, -1.0f, -2.0f);
 
         // Front face  (z = 1.0f)
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex3f( 1.0f,  1.0f, 1.0f);
-        glVertex3f(-1.0f,  1.0f, 1.0f);
-        glVertex3f(-1.0f, -1.0f, 1.0f);
-        glVertex3f( 1.0f, -1.0f, 1.0f);
+        glColor3f(red, green, blue);
+
+        glVertex3f( 1.0f, 1.0f, 2.0f);
+        glVertex3f(-1.0f, 1.0f, 2.0f);
+        glVertex3f(-1.0f, -1.0f, 2.0f);
+        glVertex3f( 1.0f, -1.0f, 2.0f);
 
         // Back face (z = -1.0f)
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex3f( 1.0f, -1.0f, -1.0f);
-        glVertex3f(-1.0f, -1.0f, -1.0f);
-        glVertex3f(-1.0f,  1.0f, -1.0f);
-        glVertex3f( 1.0f,  1.0f, -1.0f);
+        glColor3f(red, green, blue);
+
+        glVertex3f( 1.0f, -1.0f, -2.0f);
+        glVertex3f(-1.0f, -1.0f, -2.0f);
+        glVertex3f(-1.0f,  1.0f, -2.0f);
+        glVertex3f( 1.0f,  1.0f, -2.0f);
 
         // Left face (x = -1.0f)
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex3f(-1.0f,  1.0f,  1.0f);
-        glVertex3f(-1.0f,  1.0f, -1.0f);
-        glVertex3f(-1.0f, -1.0f, -1.0f);
-        glVertex3f(-1.0f, -1.0f,  1.0f);
+        glColor3f(red, green, blue);
+
+        glVertex3f(-1.0f,  1.0f,  2.0f);
+        glVertex3f(-1.0f,  1.0f, -2.0f);
+        glVertex3f(-1.0f, -1.0f, -2.0f);
+        glVertex3f(-1.0f, -1.0f,  2.0f);
 
         // Right face (x = 1.0f)
-        glColor3f(1.0f, 1.0f, 1.0f);
-        glVertex3f(1.0f,  1.0f, -1.0f);
-        glVertex3f(1.0f,  1.0f,  1.0f);
-        glVertex3f(1.0f, -1.0f,  1.0f);
-        glVertex3f(1.0f, -1.0f, -1.0f);
+        glColor3f(red, green, blue);
+
+        glVertex3f(1.0f,  1.0f, -2.0f);
+        glVertex3f(1.0f,  1.0f,  2.0f);
+        glVertex3f(1.0f, -1.0f,  2.0f);
+        glVertex3f(1.0f, -1.0f, -2.0f);
     glEnd();
 }
 
 void draw_roof() {
-
-    const float red = 0.65f;
-    const float green = 0.16f;
-    const float blue = 0.16f;
+    const float red   = 1.0f;
+    const float green = 0.0f;
+    const float blue  = 0.0f;
 
     // (x, y, z) da piramide
     glTranslatef(0.0f, 1.0f, 0.0f);
@@ -110,30 +122,30 @@ void draw_roof() {
         // Front
         glColor3f(red, green, blue);
 
-        glVertex3f( 0.0f,  1.0f, 0.0f); // vértice do topo da pirâmide
-        glVertex3f(-1.0f, -1.0f, 3.0f);
-        glVertex3f( 1.0f, -1.0f, 3.0f);
+        glVertex3f( 0.0f,  0.0f, 0.0f); // vértice do topo da pirâmide
+        glVertex3f(-1.5f, -1.0f, 2.5f);
+        glVertex3f( 1.5f, -1.0f, 2.5f);
 
         // Right
         glColor3f(red, green, blue);
 
-        glVertex3f(0.0f,  1.0f,  0.0f);
-        glVertex3f(1.0f, -1.0f,  3.0f);
-        glVertex3f(1.0f, -1.0f, -3.0f);
+        glVertex3f(0.0f,  0.0f,  0.0f);
+        glVertex3f(1.5f, -1.0f,  2.5f);
+        glVertex3f(1.5f, -1.0f, -2.5f);
 
         // Back
         glColor3f(red, green, blue);
 
-        glVertex3f( 0.0f,  1.0f,  0.0f);
-        glVertex3f( 1.0f, -1.0f, -3.0f);
-        glVertex3f(-1.0f, -1.0f, -3.0f);
+        glVertex3f( 0.0f,  0.0f,  0.0f);
+        glVertex3f( 1.5f, -1.0f, -2.5f);
+        glVertex3f(-1.5f, -1.0f, -2.5f);
 
         // Left
         glColor3f(red, green, blue);
 
-        glVertex3f( 0.0f,  1.0f,  0.0f);
-        glVertex3f(-1.0f, -1.0f, -3.0f);
-        glVertex3f(-1.0f, -1.0f,  3.0f);
+        glVertex3f( 0.0f,  0.0f,  0.0f);
+        glVertex3f(-1.5f, -1.0f, -2.5f);
+        glVertex3f(-1.5f, -1.0f,  2.5f);
     glEnd();
 }
 
@@ -142,7 +154,7 @@ void draw_house() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    glTranslatef(0.0f, 0.0f, -7.0f); // translada a casa para trás, para que ela fique visível na tela (-7.0f no eixo z)
+    glTranslatef(0.0f, 0.0f, -10.0f); // translada a casa para trás, para que ela fique visível na tela (-7.0f no eixo z)
 
     rotate_x_axis(angle_x); // rotaciona a casa em torno do eixo x por um angulo de angle_x graus
     rotate_y_axis(angle_y); // rotaciona a casa em torno do eixo y por um angulo de angle_y graus
@@ -162,27 +174,24 @@ void draw_house() {
 void handle_key_pressed(unsigned char key, int x, int y) {
     switch (key) {
         case ESCAPE_KEY:
-            exit(0);
+        case Q_KEY:
+        case Q_KEY_UPPER:
+            exit(EXIT_SUCCESS);
             break;
         case X_KEY:
+        case X_KEY_UPPER:
             angle_x += ROTATION_ANGLE;
             break;
         case Y_KEY:
+        case Y_KEY_UPPER:
             angle_y += ROTATION_ANGLE;
             break;
         case Z_KEY:
+        case Z_KEY_UPPER:
             angle_z += ROTATION_ANGLE;
             break;
-        case X_KEY_UPPER:
-            angle_x -= ROTATION_ANGLE;
-            break;
-        case Y_KEY_UPPER:
-            angle_y -= ROTATION_ANGLE;
-            break;
-        case Z_KEY_UPPER:
-            angle_z -= ROTATION_ANGLE;
-            break;
-        case 'r':
+        case R_KEY:
+        case R_KEY_UPPER:
             angle_x = 0.0f;
             angle_y = 0.0f;
             angle_z = 0.0f;
